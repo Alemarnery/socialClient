5872e61bf5ec3600fd78d83dabe732f2cc1abbf2
alemarnery <aleemar.95@gmail.com>
2021-03-17 20:25:31 -0430

    menu

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -2,18 +2,22 @@ import React from "react";
 
 const Menu = () => {
   return (
-    <div className="ui menu">
-      <a className="active item">Home</a>
-      <a className="item">Messages</a>
-      <a className="item">Friends</a>
-      <div className="right menu">
-        <div className="item">
-          <div className="ui icon input">
-            <input type="text" placeholder="Search..." />
-            <i className="search link icon"></i>
+    <div class="row">
+      <div class="twelve wide column">
+        <div className="ui menu">
+          <p className="active item">Home</p>
+          <p className="item">Messages</p>
+          <p className="item">Friends</p>
+          <div className="right menu">
+            <div className="item">
+              <div className="ui icon input">
+                <input type="text" placeholder="Search..." />
+                <i className="search link icon"></i>
+              </div>
+            </div>
+            <p className="ui item">Logout</p>
           </div>
         </div>
-        <a className="ui item">Logout</a>
       </div>
     </div>
   );
