003bd1c43c52777b3e5dbd231c8b1a18e97eb9c7
Alemarnery <aleemar.95@gmail.com>
2021-06-23 18:09:31 -0500

    cropImage

--- src/components/ImageCropper/index.js
+++ src/components/ImageCropper/index.js
@@ -6,10 +6,9 @@ const ImageCropper = ({ imageURL }) => {
   const [userImage, setUserImage] = useState(imageURL);
   const hiddenFileInput = useRef(null);
 
-  const imgRef = useRef(null);
-  const previewCanvasRef = useRef(null);
-  const [crop, setCrop] = useState({ unit: "%", width: 30, aspect: 16 / 9 });
-  const [completedCrop, setCompletedCrop] = useState(null);
+  const [image, setImage] = useState(null);
+  const [crop, setCrop] = useState({ aspect: 1 / 1 });
+  const [result, setResult] = useState(null);
 
   const handleClick = (e) => {
     e.preventDefault();
@@ -21,29 +20,13 @@ const ImageCropper = ({ imageURL }) => {
     setUserImage(URL.createObjectURL(files[0]));
   };
 
-  const onLoad = useCallback((img) => {
-    imgRef.current = img;
-  }, []);
-
-  useEffect(() => {
-    if (!completedCrop || !previewCanvasRef.current || !imgRef.current) {
-      return;
-    }
-
-    const image = imgRef.current;
-    const canvas = previewCanvasRef.current;
-    const crop = completedCrop;
-
+  function getCroppedImg(image, crop) {
+    const canvas = document.createElement("canvas");
     const scaleX = image.naturalWidth / image.width;
     const scaleY = image.naturalHeight / image.height;
+    canvas.width = crop.width;
+    canvas.height = crop.height;
     const ctx = canvas.getContext("2d");
-    const pixelRatio = window.devicePixelRatio;
-
-    canvas.width = crop.width * pixelRatio;
-    canvas.height = crop.height * pixelRatio;
-
-    ctx.setTransform(pixelRatio, 0, 0, pixelRatio, 0, 0);
-    ctx.imageSmoothingQuality = "high";
 
     ctx.drawImage(
       image,
@@ -56,7 +39,10 @@ const ImageCropper = ({ imageURL }) => {
       crop.width,
       crop.height
     );
-  }, [completedCrop]);
+
+    const base64Image = canvas.toDataURL("image/jpeg");
+    setResult(base64Image);
+  }
 
   return (
     <>
@@ -77,12 +63,12 @@ const ImageCropper = ({ imageURL }) => {
       <div>
         <ReactCrop
           src={userImage}
+          onImageLoaded={setImage}
           crop={crop}
-          onImageLoaded={onLoad}
-          onChange={(c) => setCrop(c)}
-          onComplete={(c) => setCompletedCrop(c)}
+          onChange={setCrop}
         />
-        <button onClick={(e) => e.preventDefault()}>Crop Image</button>
+        <a onClick={getCroppedImg}>Crop Image</a>
+        <img src={result} alt="Crop" />
       </div>
     </>
   );
