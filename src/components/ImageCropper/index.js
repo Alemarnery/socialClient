58269006017047c7a4054367221924155ca45af0
Alemarnery <aleemar.95@gmail.com>
2021-06-24 13:00:19 -0500

    imageCropt

--- src/components/ImageCropper/index.js
+++ src/components/ImageCropper/index.js
@@ -1,14 +1,18 @@
 import React, { useState, useRef, useCallback, useEffect } from "react";
 import ReactCrop from "react-image-crop";
-import "react-image-crop/dist/ReactCrop";
+import "react-image-crop/dist/ReactCrop.css";
 
 const ImageCropper = ({ imageURL }) => {
   const [userImage, setUserImage] = useState(imageURL);
   const hiddenFileInput = useRef(null);
-
-  const [image, setImage] = useState(null);
-  const [crop, setCrop] = useState({ aspect: 1 / 1 });
-  const [result, setResult] = useState(null);
+  const [crop, setCrop] = useState({
+    unit: "%",
+    disabled: false,
+    locked: false,
+    aspect: 16 / 9,
+    width: 50,
+    minWidth: 30,
+  });
 
   const handleClick = (e) => {
     e.preventDefault();
@@ -20,29 +24,37 @@ const ImageCropper = ({ imageURL }) => {
     setUserImage(URL.createObjectURL(files[0]));
   };
 
-  function getCroppedImg(image, crop) {
-    const canvas = document.createElement("canvas");
-    const scaleX = image.naturalWidth / image.width;
-    const scaleY = image.naturalHeight / image.height;
-    canvas.width = crop.width;
-    canvas.height = crop.height;
-    const ctx = canvas.getContext("2d");
+  //Funcion para obtener la imagen cortada. ME DA ERROR
 
-    ctx.drawImage(
-      image,
-      crop.x * scaleX,
-      crop.y * scaleY,
-      crop.width * scaleX,
-      crop.height * scaleY,
-      0,
-      0,
-      crop.width,
-      crop.height
-    );
+  //   function getCroppedImg() {
+  //     const canvas = document.createElement("canvas");
 
-    const base64Image = canvas.toDataURL("image/jpeg");
-    setResult(base64Image);
-  }
+  //     const scaleX = image.naturalWidth / image.width;
+  //     const scaleY = image.naturalHeight / image.height;
+
+  //     canvas.width = crop.width;
+  //     canvas.height = crop.height;
+  //     const ctx = canvas.getContext("2d");
+
+  //     ctx.drawImage(
+  //       image,
+  //       crop.x * scaleX,
+  //       crop.y * scaleY,
+  //       crop.width * scaleX,
+  //       crop.height * scaleY,
+  //       0,
+  //       0,
+  //       crop.width,
+  //       crop.height
+  //     );
+  //     const base64Image = canvas.toDataURL("image/jpeg");
+  //     setUserImage(base64Image);
+  //   }
+
+  const cropOnChange = (e) => {
+    console.log("crop", e);
+    setCrop(e);
+  };
 
   return (
     <>
@@ -63,12 +75,10 @@ const ImageCropper = ({ imageURL }) => {
       <div>
         <ReactCrop
           src={userImage}
-          onImageLoaded={setImage}
+          onChange={(e) => cropOnChange(e)}
           crop={crop}
-          onChange={setCrop}
         />
-        <a onClick={getCroppedImg}>Crop Image</a>
-        <img src={result} alt="Crop" />
+        <a onClick={getCroppedImg}>Crop</a>
       </div>
     </>
   );
