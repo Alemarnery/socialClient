beb74d9fcffc040bffe0e447583e76cc13c5a351
Alemarnery <aleemar.95@gmail.com>
2021-06-30 18:20:59 -0500

    solution

--- src/Pages/Login/index.js
+++ src/Pages/Login/index.js
@@ -2,10 +2,8 @@ import React from "react";
 import Form from "../../components/Form";
 import SocialButtons from "./SocialButtons";
 import FormLogin from "./FormLogin";
-import { solution } from "../../utilities/index";
 
 const Login = () => {
-  solution();
   return (
     <div className="ui middle aligned two column centered grid">
       <div className="ui card column">
